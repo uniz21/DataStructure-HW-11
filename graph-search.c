d7b14a20892641c7994cd0a3fc901fee9c0d9bdf
uniz21 <yoonyj21@naver.com>
2021-05-26 04:31:36 +0900

    함수 작성 완료

--- graph-search.c
+++ graph-search.c
@@ -3,9 +3,14 @@
 
 /* 최대 정점의 수는 10개로 설정 */
 #define MAX_VERTEX_NUM 10
+#define MAX_QUEUE_NUM 20
 
 #define TRUE 1
 #define FALSE 0
+
+int Queue[MAX_QUEUE_NUM];
+int front = -1;
+int rear = -1;
 
 /* 정점 구조체 */
 typedef struct Vertex {
@@ -29,6 +34,8 @@ void printGraph(Vertex **);
 void makeList(Vertex* v, int i);
 void DepthFirstSearch(Vertex**, int);
 void BreathFirstSearch(Vertex**, int);
+int deQueue();
+void enQueue(int aNode);
 void InitializeVisitFlag(Vertex** head);
 
 int main(void)
@@ -77,11 +84,13 @@ int main(void)
 			printf("Enter the number of the vertex to start.\n");
 			scanf("%d", &a);
 			DepthFirstSearch(&head,a);
+			InitializeVisitFlag(head);
 			break;
 		case 'b': case 'B':
 			printf("Enter the number of the vertex to start.\n");
 			scanf("%d", &a);
 			BreathFirstSearch(&head,a);
+			InitializeVisitFlag(head);
 			break;
 		case 'p': case 'P':
 			printGraph(&head);
@@ -199,17 +208,65 @@ void DepthFirstSearch(Vertex** head, int i)
 
 void BreathFirstSearch(Vertex** head, int i)
 {
+	List* p;
+	front = rear = -1;
+	printf("%5d", i);
+	head[i]->visitflag = TRUE;
+	enQueue(i);
 
+	while (1) {
+		i = deQueue(); /* dequeue */
+		if (i==-1) break;
+		for (p = head[i]; p; p = p->list)
+			if (head[p->adjNode]->visitflag == FALSE) 
+			{
+				printf("%5d", p->adjNode);
+				enQueue(p->adjNode);
+				head[p->adjNode]->visitflag = TRUE;
+			}
+	}
+}
+
+/* 큐 deQueue */
+int deQueue()
+{
+	/* 큐가 공백이면 종료 */
+	if (front == rear) return -1;/* front와 rear의 값이 같다면 큐에 어떤 노드도 없다. */
+	front++;/* 큐의 시작을 가르키는(front 방향으로 노드가 큐를 탈출(삭제)함) front값을 1만큼 증가시킨다. */
+	int temp = Queue[front];/* 삭제할 노드 저장 */
+	Queue[front] = -1;/* 노드 삭제 */
+	return temp;/* 노드 반환 */
+}
+
+/* 큐 enQueue */
+void enQueue(int aNode)
+{
+	/* 큐가 가득 찼다면 */
+	if (rear == MAX_QUEUE_NUM - 1)
+	{
+		/* 선형 큐 앞으로 끌어당기기 */
+		int size = rear - front;/* 큐에 남아있는 노드의 수를 구한다. */
+		for (int i=0;i<size;i++)/* 남은 노드 수 만큼 반복 */
+		{
+			Queue[i] = Queue[front+(i+1)];/* 노드들을 큐의 앞으로 이동시킨다*/
+			Queue[front + (i + 1)] = NULL;
+		}
+		front = -1;/* front 초기화 */
+		rear = size-1; /* rear값은 size-1로 초기화 하여 front와 rear간의 거리 유지 */
+		return;
+	}
+	rear++;/* 큐의 끝을 가르키는 rear값을 1만큼 증가시킨다.*/
+	Queue[rear] = aNode;/* 새로운 노드 삽입 */
 }
 
 void InitializeVisitFlag(Vertex** head)
 {
 	List* p;
-	for (int i = 0; i < MAX_VERTEX_NUM; i++)
+	for (int i = 0; i < MAX_VERTEX_NUM && head[i]->VertexNum!= -1; i++)
 	{
 		for (p = head[i]; p; p = p->list)
 		{
-
+			p->visitflag = FALSE;
 		}
 	}
 }
